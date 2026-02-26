86d253c2ba857d6397799498968d04730933a419
Kyrox <150089003+KyyroxxX@users.noreply.github.com>
2026-02-26 16:12:08 +0100

    Update critic_dirs_backup.sh

--- critic_dirs_backup.sh
+++ critic_dirs_backup.sh
@@ -3,6 +3,7 @@
 # Propósito: Realizar backup de carpetas importantes
 # Uso: ./backup.sh
 
+# Tienes que cambiar las siguientes variables= SRC: A lo que quieres hacerle Backup, DEST: Donde quieres que se guarde el backup, te recomiendo usar crontab para automatizarlo.
 SRC="/home/alejandro/Documentos"
 DEST="/home/alejandro/Backups"
 DATE=$(date +%Y-%m-%d_%H-%M)
